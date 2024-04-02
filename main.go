e133919140cc90e5dc08f8d0fa89473436427911
abakum <koka.abakum@gmail.com>
2024-04-02 14:51:51 +0300

    doc

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ package main
 
 import (
 	"bytes"
+	_ "embed"
 	"fmt"
 	"log"
 	"os"
@@ -19,8 +20,12 @@ import (
 
 //go:generate go run .
 
+//go:embed VERSION
+var VERSION string
+
 func main() {
 	log.SetFlags(log.Lshortfile)
+	log.Println(VERSION)
 
 	wd, err := os.Getwd()
 	if err != nil {
@@ -31,6 +36,7 @@ func main() {
 		ver = os.Args[1]
 	}
 	ver = filepath.Join(wd, ver)
+	// attrib -a VERSION
 	todo, err := version.IsA(ver, true)
 	if err != nil {
 		log.Fatalln(err)
@@ -40,6 +46,7 @@ func main() {
 		if err != nil || bytes.Count(data, []byte(".")) != 2 {
 			log.Fatalln(err)
 		}
+		// set /p VERSION=<VERSION
 		// git tag v%VERSION%-lw
 		cmd := exec.Command("git",
 			"tag",
@@ -63,6 +70,7 @@ func main() {
 	}
 
 	winres := filepath.Join(wd, "winres")
+	// attrib -a winres\*
 	todo, err = version.IsA(winres, true)
 	if err != nil || !todo {
 		return
