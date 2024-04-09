69048859f816992c432f2785877533ff38fc9324
abakum <koka.abakum@gmail.com>
2024-04-09 17:07:28 +0300

    doc

--- main.go
+++ main.go
@@ -1,5 +1,8 @@
-// Add `//go:generate version` to `main.go` so that changes in the `VERSION` file or in the 'winres' directory affect the result of 'go build'.
-// Добавь `//go:generate version` в `main.go` чтоб изменения в файле `VERSION` или в каталоге `winres` учитывались при `go build`.
+// Add `//go:generate version` or `//go:generate github.com/abakum/version` to `main.go` so that changes in the `VERSION` file
+// and for `Windows` in the 'winres' directory affect the result of 'go build'. After the changes and before `go build`, run `go generate`.
+
+// Добавь `//go:generate version` или `//go:generate github.com/abakum/version` в `main.go` чтоб изменения в файле `VERSION`
+// а для `Windows` и в каталоге `winres` учитывались при `go build`. После изменений и перед `go build` запускай `go generate`.
 package main
 
 import (
